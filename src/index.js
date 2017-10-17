3197e29b5a766e0f0580dfb12c3f70f2d5eea4a8
elona2011 <cdszbjj@qq.com>
2017-10-18 01:01:09 +0800

    u

--- src/index.js
+++ src/index.js
@@ -1,18 +1,78 @@
 class MovingObject {
-    constructor(dom) {
+    constructor(dom, a, b, step) {
         this.dom = dom;
-    }
-    addOrbit(orbit) {
-        this.orbit = orbit;
+        this.a = a;
+        this.b = b;
+        this.step = step;
+        this.direction = true;
+        this.x = 0;
+        this.y = this.getY(this.x);
     }
     moving() {
-        this.dom.style['transform'] = 'translate(100px,100px)';
+        setInterval(() => {
+            let x = 400, y = 400;
+            this.tick();
+            this.dom.style['transform'] = `matrix(1,0,0,1,${x + this.x},${y + this.y})`;
+        }, 15);
+    }
+    tick() {
+        let step = 1;
+        step = this.loop(0, step);
+        let { x, y } = this.getXY(step);
+        if (y * this.y < 0)
+            this.direction = !this.direction;
+        this.x = x;
+        this.y = y;
+    }
+    loop(min, max) {
+        let step = (min + max) / 2;
+        let d = this.getD(step);
+        if (d - 1 > 0.5) {
+            return this.loop(min, step);
+        }
+        else if (d - 1 < -0.5) {
+            return this.loop(step, max);
+        }
+        else {
+            return step;
+        }
+    }
+    getD(step) {
+        let { x, y } = this.getXY(step);
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+    getXY(step) {
+        let x = 0, y = this.getY(x);
+        if (this.direction) {
+            x = this.x + step;
+            if (x > this.a) {
+                x = this.x;
+                y = -this.getY(x);
+            }
+        }
+        else {
+            x = this.x - step;
+            if (x < -this.a) {
+                x = this.x;
+                y = -this.getY(x);
+            }
+        }
+        y = this.getY(x);
+        return { x, y };
+    }
+    getY(x) {
+        let y = Math.sqrt((1 - x * x / (this.a * this.a)) * this.b * this.b);
+        if (this.direction) {
+            return -y;
+        }
+        else
+            return y;
     }
 }
 let domEarth = document.querySelector('svg #earth');
 let earth;
 if (domEarth) {
-    earth = new MovingObject(domEarth);
+    earth = new MovingObject(domEarth, 300, 200, 1);
     earth.moving();
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,80 @@
-interface location {
-    x: number,
-    y: number
-}
-
 class MovingObject {
-    public orbit: location[]
+    public direction: boolean = true
+    public x: number = 0
+    public y: number = this.getY(this.x)
 
-    constructor(public dom: SVGElement) {
+    constructor(public dom: SVGElement, public a: number, public b: number, public step: number) {
 
-    }
-
-    addOrbit(orbit: location[]) {
-        if (orbit.length < 1) throw new Error('orbit.length<1');
-
-        this.orbit = orbit
     }
 
     moving() {
-        this.dom.style['transform'] = 'translate(100px,100px)'
+        setInterval(() => {
+            let x = 400,
+                y = 400
+            this.tick()
+            this.dom.style['transform'] = `matrix(1,0,0,1,${x + this.x},${y + this.y})`
+        }, 15)
+    }
+
+    tick(): void {
+        let step = 1
+
+        step = this.loop(0, step)
+
+        let { x, y } = this.getXY(step)
+        if (y * this.y < 0) this.direction = !this.direction
+
+        this.x = x
+        this.y = y
+
+    }
+
+    loop(min: number, max: number): number {
+        let step = (min + max) / 2
+        let d = this.getD(step)
+        if (d - 1 > 0.5) {
+            return this.loop(min, step)
+        } else if (d - 1 < -0.5) {
+            return this.loop(step, max)
+        } else {
+            return step
+        }
+    }
+
+    getD(step: number): number {
+        let { x, y } = this.getXY(step)
+
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y))
+    }
+
+    getXY(step: number): { x: number, y: number } {
+        let x = 0, y = this.getY(x)
+
+        if (this.direction) {
+            x = this.x + step
+            if (x > this.a) {
+                x = this.x
+                y = -this.getY(x)
+                // this.direction = !this.direction
+            }
+        } else {
+            x = this.x - step
+            if (x < -this.a) {
+                x = this.x
+                y = -this.getY(x)
+                // this.direction = !this.direction
+            }
+        }
+        y = this.getY(x)
+        return { x, y }
+    }
+
+    getY(x: number): number {
+        let y = Math.sqrt((1 - x * x / (this.a * this.a)) * this.b * this.b)
+        if (this.direction) {
+            return -y
+        } else
+            return y
     }
 }
 
@@ -26,6 +83,6 @@ let domEarth = <SVGElement>document.querySelector('svg #earth')
 let earth: MovingObject
 
 if (domEarth) {
-    earth = new MovingObject(domEarth)
+    earth = new MovingObject(domEarth, 300, 200, 1)
     earth.moving()
 }
