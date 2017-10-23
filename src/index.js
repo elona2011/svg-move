44232881471c32507852cd88b7907d7ac2c8d82e
elona2011 <cdszbjj@qq.com>
2017-10-24 00:13:48 +0800

    u

--- src/index.js
+++ src/index.js
@@ -6,11 +6,25 @@ class MovingObject {
         this.step = step;
         this.direction = true;
         this.x = 0;
+        this.y = 0;
+    }
+    moving() {
+        setInterval(() => {
+            let x = 400, y = 400;
+            this.tick();
+            this.dom.style['transform'] = `matrix(1,0,0,1,${x + this.x},${y + this.y})`;
+        }, 15);
+    }
+    tick() {
+        let step = 1;
+        this.x = 200;
+        this.y = 200;
     }
 }
 let domEarth = document.querySelector('svg #earth');
 let earth;
 if (domEarth) {
     earth = new MovingObject(domEarth, 300, 200, 1);
+    earth.moving();
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 class MovingObject {
     public direction: boolean = true
     public x: number = 0
-    // public y: number = this.getY(this.x)
+    public y: number = 0
 
     constructor(public dom: SVGElement, public a: number, public b: number, public step: number) {
 
     }
 
-    // moving() {
-    //     setInterval(() => {
-    //         let x = 400,
-    //             y = 400
-    //         this.tick()
-    //         this.dom.style['transform'] = `matrix(1,0,0,1,${x + this.x},${y + this.y})`
-    //     }, 15)
-    // }
+    moving() {
+        setInterval(() => {
+            let x = 400,
+                y = 400
+            this.tick()
+            this.dom.style['transform'] = `matrix(1,0,0,1,${x + this.x},${y + this.y})`
+        }, 15)
+    }
 
-    // tick(): void {
-    //     let step = 1
+    tick(): void {
+        let step = 1
 
-    //     step = this.loop(0, step)
+        // step = this.loop(0, step)
 
-    //     let { x, y } = this.getXY(step)
-    //     if (y * this.y < 0) this.direction = !this.direction
+        // let { x, y } = this.getXY(step)
+        // if (y * this.y < 0) this.direction = !this.direction
 
-    //     this.x = x
-    //     this.y = y
+        this.x = 200
+        this.y = 200
 
-    // }
+    }
 
     // loop(min: number, max: number): number {
     //     let step = (min + max) / 2
@@ -84,5 +84,5 @@ let earth: MovingObject
 
 if (domEarth) {
     earth = new MovingObject(domEarth, 300, 200, 1)
-    // earth.moving()
+    earth.moving()
 }
